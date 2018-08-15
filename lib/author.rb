27807028cf36bbae2387e14a0ff16c2979534b4e
Adam Duff <j.adam.duff@gmail.com>
2018-08-15 10:21:43 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -3,5 +3,21 @@ class Author
 
   def initialize
     @name = name
+    @posts = []
+  end
+
+  def posts
+    @posts
+  end
+
+  def add_post(post)
+    post.author = self
+    @posts << post
+  end
+
+  def add_post_by_title(title)
+    post = Post.new(title)
+    post.author = self
+    @posts << post
   end
 end
