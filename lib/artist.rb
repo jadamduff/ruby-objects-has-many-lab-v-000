79df987e82ebdf9610ba7d29cf2c1163955c3bf6
Adam Duff <j.adam.duff@gmail.com>
2018-08-15 09:38:29 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,6 +2,13 @@ class Artist
   attr_accessor :name
   def initialize(name)
     @name = name
-    @songs = []
+  end
+
+  def songs(songs)
+    @songs = songs
+  end
+
+  def songs
+    @songs
   end
 end
